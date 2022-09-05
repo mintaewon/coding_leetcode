27c7e27455bd6e04165bf5756fbda05ee0f7171a
mintaewon <taewon1230@naver.com>
2022-09-05 13:50:55 +0900

    add p1

---
+++ 0905/P1_taewon.py
@@ -0,0 +1,27 @@
+''''
+
+투포인터로 접근했으나 시간초과 간당간당함
+
+밑에 O(n^2) 보다 빠른 방법을 사용해야된다고 나와있음
+
+'''
+
+# 투포인터 - 시간초과
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for start in range(len(nums)-1):
+            end=start+1
+            while end<len(nums):
+                if nums[start]+nums[end] == target:
+                    return [start, end]
+                end+=1
+
+
+# target에서 빼준 값을 저장 후 공백으로 만들어 in 연산
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            temp = target-nums[i]
+            nums[i] = None
+            if temp in nums:
+                return [i, nums.index(temp)]

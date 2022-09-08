ed1fe9c6158cb3293ad589c403eb32913bff20fc
mintaewon <taewon1230@naver.com>
2022-09-08 09:59:05 +0900

    add p15

--- 0908/P15_taewon.py
+++ 0908/P15_taewon.py
@@ -1,6 +1,6 @@
 class Solution:    
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        # 노드가 없을때 0 반환
-        if root is None:
+        if root:
+            return max(self.maxDepth(root.left), self.maxDepth(root.right))+1
+        else:
             return 0
-        return max(self.maxDepth(root.left), self.maxDepth(root.right))+1
